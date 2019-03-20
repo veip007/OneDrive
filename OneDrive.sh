13d096c0c6ed1244948d1acd757bae5b4a110d38
veip007 <12257917+veip007@users.noreply.github.com>
2019-03-20 13:43:59 +0800

    Update OneDrive.sh

--- OneDrive.sh
+++ OneDrive.sh
@@ -3,12 +3,12 @@
 [ -d /usr/local/etc/OneDrive ] && mv -f /usr/local/etc/OneDrive /usr/local/etc/OneDrive.bak
 mkdir -p /usr/local/etc/OneDrive
 cd /usr/local/etc/OneDrive
-wget --no-check-certificate -q -O json-parser "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/json-parser"
-wget --no-check-certificate -q -O onedrive "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive"
-wget --no-check-certificate -q -O onedrive-d "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-d"
-wget --no-check-certificate -q -O onedrive-authorize "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-authorize"
-wget --no-check-certificate -q -O onedrive-base "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive-base"
-wget --no-check-certificate -q -O onedrive.cfg "https://raw.githubusercontent.com/0oVicero0/OneDrive/master/Business/onedrive.cfg"
+wget --no-check-certificate -q -O json-parser "https://raw.githubusercontent.com/veip007/OneDrive/master/Business/json-parser"
+wget --no-check-certificate -q -O onedrive "https://raw.githubusercontent.com/veip007/OneDrive/master/Business/onedrive"
+wget --no-check-certificate -q -O onedrive-d "https://raw.githubusercontent.com/veip007/OneDrive/master/Business/onedrive-d"
+wget --no-check-certificate -q -O onedrive-authorize "https://raw.githubusercontent.com/veip007/OneDrive/master/Business/onedrive-authorize"
+wget --no-check-certificate -q -O onedrive-base "https://raw.githubusercontent.com/veip007/OneDrive/master/Business/onedrive-base"
+wget --no-check-certificate -q -O onedrive.cfg "https://raw.githubusercontent.com/veip007/OneDrive/master/Business/onedrive.cfg"
 chmod -R a+x /usr/local/etc/OneDrive
 ln -sf /usr/local/etc/OneDrive/onedrive /usr/local/bin/
 ln -sf /usr/local/etc/OneDrive/onedrive-d /usr/local/bin/
